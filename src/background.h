52acaff12e8b156f91914236abcadf91238ba39e
Your Name <laudrup@stacktrace.dk>
2021-03-20 00:40:07 +0100

    Load background images as separate sprites
    To limit the risk of hitting the max texture size load the background
    image as a single file and load individual textures from there.
    
    Also remove use of ResourceHolder which doesn't really seem to make
    much sense for now.

--- src/background.h
+++ src/background.h
@@ -8,15 +8,14 @@
 class Background : public sf::Drawable
 {
 public:
-  Background(const sf::Vector2u& area,
-             thor::ResourceHolder<sf::Texture, std::string>& resources);
+  Background(const sf::Vector2u& area);
   sf::Vector2f scale() const { return sprite_.getScale(); }
   sf::Vector2f size() const {
     return {sprite_.getGlobalBounds().width, sprite_.getGlobalBounds().height}; }
 
 private:
   void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-  std::vector<sf::IntRect> textures_;
+  std::vector<sf::Texture> textures_;
   sf::Sprite sprite_;
 };
 
